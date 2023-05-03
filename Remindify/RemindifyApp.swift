bfae09bacc1f8f29404e733532301afe65748bcd
Anjie Yang <digmouse233@gmail.com>
2023-05-03 16:28:18 -0400

    Initial Commit

---
+++ Remindify/RemindifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindifyApp.swift
+//  Remindify
+//
+//  Created by digmouse on 2023-05-03.
+//
+
+import SwiftUI
+
+@main
+struct RemindifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
